9bb480b2f8bbc40ce3bd3163fdd81909c3657fb0
s <s@s.s>
2019-11-20 11:37:52 +0300

    Express app with serverless working well on local machine

--- src/api/router.js
+++ src/api/router.js
@@ -3,7 +3,10 @@ const router = express.Router()
 
 const todos = [
     {id: 1, name: 'clean up', status: 'open'},
-    {id: 2, name: 'cook', status: 'done'}
+    {id: 2, name: 'cook', status: 'done'},
+    {id: 3, name: "Chicken", status: "open"},
+    {id: 4, name: "Potatoes", status: "done"},
+    {id: 5, name: 'Reading', status: 'In progress'}
 ]  
 
 router.get('/', (req, res) => res.send("Welcome to todos"))
